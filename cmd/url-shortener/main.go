c4e52877f2498343ab03461b5a33fbfdb1a4b873
Sekul <wojowniksek@gmail.com>
2024-10-30 23:44:48 +0100

    tweak(cmd): print on http server start

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -14,6 +14,7 @@ func main() {
 	router := adapters.NewRouter(server)
 	http.Handle("/", router)
 
+	log.Printf("Starting server on port %s", os.Getenv("HTTP_PORT"))
 	err := http.ListenAndServe(os.Getenv("HTTP_PORT"), nil)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
